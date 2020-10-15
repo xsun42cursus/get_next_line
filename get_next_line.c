8522aa53ea2575167849ad0057a08ecf36e7c7b9
xiaobai <s.son@sum.sd.keio.ac.jp>
2020-10-15 13:26:20 +0900

    implemented getline

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: xsun <xiaobai@student.42tokyo.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/13 23:45:25 by xsun              #+#    #+#             */
-/*   Updated: 2020/10/14 10:27:21 by xsun             ###   ########.fr       */
+/*   Updated: 2020/10/15 13:24:53 by xsun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,88 +14,129 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-//int		get_next_line(int fd, char **line)
-//{
-//	size_t res;
-//	static char buf[BUFFER_SIZE + 1];
-//
-//	if (fd < 0)
-//		return (-1);
-//	while (1)
-//	{
-//		res = read(fd, buf, BUFFER_SIZE);
-//		if (res == -1)
-//			return (0);
-//	}
-//	printf("%s\n", buf);
-//
-//	return (res);
-//}
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 0
+#endif
 
-size_t ft_strlen(const char *s)
+static char *ft_strdup(const char *s)
 {
-	size_t i;
-
-	i = 0;
-	while (*s)
-	{
-		s++;
-		i++;
-	}
-	return (i);
-
-}
-
-int append(char **dst, char *src)
-{
-	size_t dstsize = ft_strlen(*dst);
-	size_t srcsize = ft_strlen(src);
+	size_t size;
+	char *ret;
 	char *head;
 
-	head = *dst;
-	if ((*dst = malloc(sizeof(char) * (dstsize + srcsize + 1))) == NULL)
-		return (-1);
-	int i = 0;
-	while (i < dstsize)
+	size = 0;
+	while (s[size])
+		size++;
+	if((ret = malloc(sizeof(char) * (size + 1))) == NULL)
+		return (NULL);
+	head = ret;
+	while (*s)
+		*head++ = *s++;
+	*head = '\0';
+	return (ret);
+
+}
+
+static void ft_move(char *dst, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i])
 	{
-		(*dst)[i] = head[i];
+		dst[i] = src[i];
 		i++;
 	}
-	free(head);
-	while (*src)
-	{
-		(*dst)[i++] = *src++;
-	}
-	(*dst)[i] = '\0';
-	return (0);
+	dst[i] = '\0';
 }
+
+static int hasendl(char *line, char *buf, size_t *start)
+{
+	size_t i;
+	size_t offset;
+
+	i = 0;
+	while (buf[i])
+	{
+		line[*start + i] = buf[i];
+		if (buf[i++] == '\n')
+		{
+			ft_move(&buf[0], &buf[i]);
+			*start = *start + i;
+			line[*start] = '\0';
+			return (1);
+		}
+	}
+	offset = i;
+	*start = *start + i;
+	line[*start] = '\0';
+	return (0);
+
+}
+
+int		get_next_line(int fd, char **line)
+{
+	size_t res;
+	static char buf[BUFFER_SIZE + 1];
+	char *tmp;
+	size_t readpoint;
+
+	if (fd < 0)
+		return (-1);
+
+	if ((tmp = malloc(sizeof(char) * (100000000))) == NULL)
+		return (-1);
+	readpoint = 0;
+	if (hasendl(tmp, buf, &readpoint))
+	{
+		*line= ft_strdup(tmp);
+		printf("buf: %s\n", buf);
+		free(tmp);
+		return (1);
+	}
+	while (1)
+	{
+		res = read(fd, buf, BUFFER_SIZE);
+		if (res != 0 && hasendl(tmp, buf, &readpoint))
+		{
+			*line= ft_strdup(tmp);
+			free(tmp);
+			if (res == 0)
+				return (0);
+			return (1);
+		}
+		else if (res == 0)
+		{
+			*line= ft_strdup("");
+			free(tmp);
+			return (0);
+		}
+		else if (res == -1)
+		{
+			free(tmp);
+			return (-1);
+		}
+	}
+	free(tmp);
+
+	return (res);
+}
+
 
 int main(void)
 {
-	char *src[10] = {"aahw", "hwllo", "good", "bad", "no", "aaaaaaaaaaaa"};
-	char *dst = malloc(sizeof(char) * (ft_strlen(src[0]) + 1));
-	char *s = src[0];
-	int i = 0;
-	int size = 3000;
-	while (*s)
-		dst[i++] = *s++;
-	dst[i] = '\0';
+	int fd;
 
-	i = 0;
-	int j = 0;
-	while (j < size)
+	fd = open("./hello.txt", O_RDONLY);
+	if (fd < 0)
+		return (-1);
+
+	char *line;
+
+	while (get_next_line(0, &line))
 	{
-		while (i < 6)
-		{
-			if (append(&dst, src[i++]))
-				printf("!!!!!!!!!!!not!!!!!!!!!!!\n");
-		}
-		i = 0;
-		j++;
+		printf("line: %s", line);
 	}
 
-	printf("%s\n", dst);
-	//while(1)
-	//{}
-	return 0;
+	return (0);
 }
